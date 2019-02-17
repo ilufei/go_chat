80ff9c8464e442ed286c71a4217a816da434f43d
张月建 <277295746@qq.com>
2019-02-17 21:57:35 +0800

    update

--- websocket.go
+++ websocket.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"flag"
 	"log"
-	"os"
-	"os/signal"
 	"time"
+	"encoding/json"
 	"net/http"
-	_ "ichat/lib"
+	"ichat/lib"
 	"github.com/gorilla/websocket"
 )
 
 var addr = flag.String("addr", "0.0.0.0:9501", "http service address")
 var upgrader = websocket.Upgrader{} // use default options
 var conns []*websocket.Conn
+
+var message map[string]interface{}
+type result struct {
+	Roomid string
+	Data []map[string]interface{}
+}
+
+func main() {
+	go postMessage()
+
+	flag.Parse()
+	http.HandleFunc("/echo", echo)
+	http.ListenAndServe(*addr, nil)
+}
 
 func echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
@@ -41,44 +55,86 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func main() {
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-
-	go postMessage(interrupt)
-
-	flag.Parse()
-	http.HandleFunc("/echo", echo)
-	http.ListenAndServe(*addr, nil)
-}
-
-func postMessage(interrupt chan os.Signal) {
+func postMessage() {
 	for {
-		select {
-			case <-interrupt :
-				log.Println("postMessage interrupt by main")
-				return
-			default : 
-				message := getMessage()
+		roomids := getRoomIds()
+		for _, roomid := range roomids {
+			data := []map[string]interface{}{}	//清空数据
+			allMessage := getMessage(roomid)
+			if len(allMessage) == 0 {
+				continue
+			}
 
-				for _, conn := range conns {
-					conn.WriteMessage(websocket.TextMessage, []byte(message))
-				}
+			for _, messageJson := range allMessage {
+				if err := json.Unmarshal([]byte(messageJson), &message); err != nil {
+					continue
+			    }
 
-				time.Sleep(time.Second)
+			    data = append(data, message)
+			}
+
+			resultObject := result{roomid, data,}
+			resultJson, _ := json.Marshal(resultObject)
+
+			for _, conn := range conns {
+				conn.WriteMessage(websocket.TextMessage, []byte(resultJson))
+			}
 		}
+
+		time.Sleep(time.Second)
 	}
 }
 
 
-func getMessage() (string) {
-	/*
-	redisKey := Sprintf("%s_1", roomid)
-	message, err := lib.RedisClient.LPop(redisKey).Result()
-	*/
+func getMessage(roomid string) ([]string) {
+	var allMessages []string 
+	var messages []string
+	var messageLength int64 = 50
+	
+	room1Key := fmt.Sprintf("%s_1", roomid)
+	room2Key := fmt.Sprintf("%s_2", roomid)
+	room3Key := fmt.Sprintf("%s_3", roomid)
 
-	data := "this is message"
-	return data
+	messages, _ = lib.RedisClient.LRange(room1Key, 0, messageLength).Result()
+	if len(messages) != 0 {
+		allMessages = append(allMessages, messages...)
+		messageLength = messageLength - int64(len(messages))
+
+		if messageLength <= 0 {
+			_, _ = lib.RedisClient.Del(room1Key, room2Key, room3Key).Result()
+			return allMessages
+		}
+	}
+	
+	messages, _ = lib.RedisClient.LRange(room2Key, 0, messageLength).Result()
+	if len(messages) != 0 {
+		allMessages = append(allMessages, messages...)
+		messageLength = messageLength - int64(len(messages))
+
+		if messageLength <= 0 {
+			_, _ = lib.RedisClient.Del(room1Key, room2Key, room3Key).Result()
+			return allMessages
+		}
+	}
+
+	messages, _ = lib.RedisClient.LRange(room3Key, 0, messageLength).Result()
+	if len(messages) != 0 {
+		allMessages = append(allMessages, messages...)
+		messageLength = messageLength - int64(len(messages))
+	}
+
+	_, _ = lib.RedisClient.Del(room1Key, room2Key, room3Key).Result()
+	return allMessages
+}
+
+func getRoomIds() ([]string) {
+	roomids, err := lib.RedisClient.SMembers("ichat_roomids").Result()
+
+	if err != nil {
+		return []string{}
+	}
+
+	return roomids
 }
 
 
